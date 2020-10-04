60792a946927a4e107bc4fb68045a7a0dabc0e7e
martinontcode <martin.onton@gmail.com>
2020-10-04 21:06:00 +0300

    Navbar, Sidebar
    * Created Navbar component.
    * Created Sidebar component.
    
    * Added custom Navbar styling.
    * Added custom Sidebar styling.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './menu/navbar/navbar.component';
+import { SidebarComponent } from './menu/sidebar/sidebar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    SidebarComponent
   ],
   imports: [
     BrowserModule,
